de83bc809170fe4a5fec03d3d1710f2c586beb26
Avery Crespi <averycrespi@gmail.com>
2019-08-05 16:19:30 -0700

    Finish Function::new

--- src/error.rs
+++ src/error.rs
@@ -7,13 +7,19 @@ use std::fmt;
 #[derive(Debug, Clone)]
 pub enum YolkError {
     NotImplemented,
+
     DuplicateImport(String),
     DuplicateExport(String),
     ExistingImport(String),
-    ExistingFunction(String),
     ExistingVariable(String),
     UndefinedVariable(String),
+
+    DuplicateParams(String),
+    ExistingFunction(String),
+    RecursiveCall(String),
     UndefinedFunction(String),
+    UndefinedLocalVariable { function: String, variable: String },
+
     MismatchingArrays(InfixOp),
     NestedArrays,
 }
@@ -24,19 +30,33 @@ impl fmt::Display for YolkError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             YolkError::NotImplemented => write!(f, "not implemented"),
+
             YolkError::DuplicateImport(ident) => write!(f, "duplicate import: {}", ident),
             YolkError::DuplicateExport(ident) => write!(f, "duplicate export: {}", ident),
             YolkError::ExistingImport(ident) => {
                 write!(f, "cannot import existing variable: {}", ident)
             }
-            YolkError::ExistingFunction(ident) => {
-                write!(f, "cannot redefine existing function: {}", ident)
-            }
             YolkError::ExistingVariable(ident) => {
                 write!(f, "cannot reassign existing variable: {}", ident)
             }
             YolkError::UndefinedVariable(ident) => write!(f, "undefined variable: {}", ident),
+
+            YolkError::DuplicateParams(ident) => {
+                write!(f, "function has duplicate parameters: {}", ident)
+            }
+            YolkError::ExistingFunction(ident) => {
+                write!(f, "cannot redefine existing function: {}", ident)
+            }
+            YolkError::RecursiveCall(ident) => {
+                write!(f, "cannot recursively call function: {}", ident)
+            }
             YolkError::UndefinedFunction(ident) => write!(f, "undefined function: {}", ident),
+            YolkError::UndefinedLocalVariable { function, variable } => write!(
+                f,
+                "variable: {} is undefined in scope of function: {}",
+                variable, function
+            ),
+
             YolkError::MismatchingArrays(op) => {
                 write!(f, "cannot apply operation to mismatching arrays: {:?}", op)
             }
--- src/function.rs
+++ src/function.rs
@@ -1,15 +1,74 @@
 use crate::ast::YolkNode;
 use crate::error::YolkError;
 
+use std::collections::HashSet;
+
 /// Represents a Yolk function.
 #[derive(Debug, Clone)]
-pub struct Function {}
+pub struct Function {
+    params: Vec<String>,
+    body: YolkNode,
+}
 
 impl Function {
     /// Creates a new Yolk function.
     pub fn new(ident: &str, params: Vec<String>, body: &YolkNode) -> Result<Function, YolkError> {
-        //TODO: implement
-        Err(YolkError::NotImplemented)
+        Function::check_params(ident, params.clone())?;
+        Function::check_calls(ident, body)?;
+        Function::check_locals(&ident, params.clone(), body)?;
+        Ok(Function {
+            params: params.clone(),
+            body: body.clone(),
+        })
+    }
+
+    fn check_params(ident: &str, params: Vec<String>) -> Result<(), YolkError> {
+        let mut set = HashSet::new();
+        let is_unique = params.iter().all(move |x| set.insert(x));
+        if is_unique {
+            Ok(())
+        } else {
+            Err(YolkError::DuplicateParams(ident.to_string()))
+        }
+    }
+
+    fn check_calls(ident: &str, body: &YolkNode) -> Result<(), YolkError> {
+        let mut calls = Vec::new();
+        body.find(&mut calls, &|n| match n {
+            YolkNode::CallExpr { ident: _, args: _ } => true,
+            _ => false,
+        });
+        for call in calls.iter() {
+            if let YolkNode::CallExpr {
+                ident: call_ident,
+                args: _,
+            } = call
+            {
+                if ident == call_ident {
+                    return Err(YolkError::RecursiveCall(ident.to_string()));
+                }
+            }
+        }
+        Ok(())
+    }
+
+    fn check_locals(ident: &str, params: Vec<String>, body: &YolkNode) -> Result<(), YolkError> {
+        let mut locals = Vec::new();
+        body.find(&mut locals, &|n| match n {
+            YolkNode::Ident(_) => true,
+            _ => false,
+        });
+        for local in locals.iter() {
+            if let YolkNode::Ident(local_ident) = local {
+                if !params.contains(local_ident) {
+                    return Err(YolkError::UndefinedLocalVariable {
+                        function: ident.to_string(),
+                        variable: local_ident.to_string(),
+                    });
+                }
+            }
+        }
+        Ok(())
     }
 
     /// Calls a function with arguments.
