922017e2722769b11d410e52f5aeede982481530
Avery Crespi <averycrespi@gmail.com>
2019-08-06 11:06:47 -0700

    Finish Function::call

--- src/error.rs
+++ src/error.rs
@@ -6,26 +6,20 @@ use std::fmt;
 /// Represents a general Yolk error.
 #[derive(Debug, Clone)]
 pub enum YolkError {
-    NotImplemented,
-
-    DuplicateImport(String),
     DuplicateExport(String),
-    ExistingImport(String),
-
-    ExistingVariable(String),
-    UndefinedVariable(String),
-
-    ImportedExport(String),
-
+    DuplicateImport(String),
     DuplicateParams(String),
-    RecursiveCall(String),
-
     ExistingFunction(String),
-    UndefinedFunction(String),
-    UndefinedLocalVariable { function: String, variable: String },
-
+    ExistingImport(String),
+    ExistingVariable(String),
+    ImportedExport(String),
     MismatchingArrays(InfixOp),
     NestedArrays,
+    RecursiveCall(String),
+    UndefinedFunction(String),
+    UndefinedLocalVariable { function: String, variable: String },
+    UndefinedVariable(String),
+    WrongNumberOfArgs(String),
 }
 
 impl error::Error for YolkError {}
@@ -33,27 +27,31 @@ impl error::Error for YolkError {}
 impl fmt::Display for YolkError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            YolkError::NotImplemented => write!(f, "not implemented"),
-            YolkError::DuplicateImport(ident) => write!(f, "duplicate import: {}", ident),
             YolkError::DuplicateExport(ident) => write!(f, "duplicate export: {}", ident),
+            YolkError::DuplicateImport(ident) => write!(f, "duplicate import: {}", ident),
+            YolkError::DuplicateParams(ident) => {
+                write!(f, "function: {} has duplicate parameters", ident)
+            }
+            YolkError::ExistingFunction(ident) => {
+                write!(f, "cannot redefine existing function: {}", ident)
+            }
             YolkError::ExistingImport(ident) => {
                 write!(f, "cannot import existing variable: {}", ident)
-            }
-            YolkError::ImportedExport(ident) => {
-                write!(f, "cannot export imported variable: {}", ident)
             }
             YolkError::ExistingVariable(ident) => {
                 write!(f, "cannot reassign existing variable: {}", ident)
             }
-            YolkError::UndefinedVariable(ident) => write!(f, "undefined variable: {}", ident),
-            YolkError::DuplicateParams(ident) => {
-                write!(f, "function has duplicate parameters: {}", ident)
+            YolkError::ImportedExport(ident) => {
+                write!(f, "cannot export imported variable: {}", ident)
             }
+            YolkError::MismatchingArrays(op) => write!(
+                f,
+                "cannot apply operation: {:?} to arrays of different lengths",
+                op
+            ),
+            YolkError::NestedArrays => write!(f, "cannot nest arrays"),
             YolkError::RecursiveCall(ident) => {
                 write!(f, "cannot recursively call function: {}", ident)
-            }
-            YolkError::ExistingFunction(ident) => {
-                write!(f, "cannot redefine existing function: {}", ident)
             }
             YolkError::UndefinedFunction(ident) => write!(f, "undefined function: {}", ident),
             YolkError::UndefinedLocalVariable { function, variable } => write!(
@@ -61,10 +59,10 @@ impl fmt::Display for YolkError {
                 "variable: {} is undefined in scope of function: {}",
                 variable, function
             ),
-            YolkError::MismatchingArrays(op) => {
-                write!(f, "cannot apply operation to mismatching arrays: {:?}", op)
+            YolkError::UndefinedVariable(ident) => write!(f, "undefined variable: {}", ident),
+            YolkError::WrongNumberOfArgs(ident) => {
+                write!(f, "wrong number of arguments for function: {}", ident)
             }
-            YolkError::NestedArrays => write!(f, "cannot nest arrays"),
         }
     }
 }
--- src/function.rs
+++ src/function.rs
@@ -19,12 +19,12 @@ impl Function {
             params: params.to_vec(),
             body: body.clone(),
         };
-        function.check_params()?;
-        function.check_node(&function.body)?;
+        function.check_for_duplicate_params()?;
+        function.check_body_node(&function.body)?;
         Ok(function)
     }
 
-    fn check_params(&self) -> Result<(), YolkError> {
+    fn check_for_duplicate_params(&self) -> Result<(), YolkError> {
         let mut uniq = HashSet::new();
         if self.params.iter().all(move |x| uniq.insert(x)) {
             Ok(())
@@ -33,21 +33,23 @@ impl Function {
         }
     }
 
-    fn check_node(&self, node: &YolkNode) -> Result<(), YolkError> {
+    fn check_body_node(&self, node: &YolkNode) -> Result<(), YolkError> {
         match node {
-            YolkNode::PrefixExpr { op: _, expr } => self.check_node(expr)?,
-            YolkNode::CallExpr { ident: id, args } => {
+            YolkNode::PrefixExpr { op: _, expr } => self.check_body_node(expr)?,
+            // Check for recursive calls
+            YolkNode::CallExpr { ident, args } => {
                 for arg in args.iter() {
-                    self.check_node(arg)?;
+                    self.check_body_node(arg)?;
                 }
-                if self.ident == id.to_string() {
+                if self.ident == ident.to_string() {
                     return Err(YolkError::RecursiveCall(self.ident.to_string()));
                 }
             }
             YolkNode::InfixExpr { lhs, op: _, rhs } => {
-                self.check_node(lhs)?;
-                self.check_node(rhs)?;
+                self.check_body_node(lhs)?;
+                self.check_body_node(rhs)?;
             }
+            // Check for undefined local variables
             YolkNode::Ident(s) => {
                 if !self.params.contains(s) {
                     return Err(YolkError::UndefinedLocalVariable {
@@ -58,7 +60,7 @@ impl Function {
             }
             YolkNode::Array(exprs) => {
                 for expr in exprs.iter() {
-                    self.check_node(expr)?;
+                    self.check_body_node(expr)?;
                 }
             }
             _ => (),
@@ -67,10 +69,55 @@ impl Function {
     }
 
     /// Calls a function with arguments.
-    ///
-    /// Returns a YolkNode where the function parameters have been replaced with arguments.
     pub fn call(&self, args: &[YolkNode]) -> Result<YolkNode, YolkError> {
-        //TODO: implement
-        Err(YolkError::NotImplemented)
+        if self.params.len() != args.len() {
+            Err(YolkError::WrongNumberOfArgs(self.ident.to_string()))
+        } else {
+            self.replace_params_with_args(args, &self.body)
+        }
+    }
+
+    fn replace_params_with_args(
+        &self,
+        args: &[YolkNode],
+        node: &YolkNode,
+    ) -> Result<YolkNode, YolkError> {
+        match node {
+            YolkNode::PrefixExpr { op, expr } => Ok(YolkNode::PrefixExpr {
+                op: op.clone(),
+                expr: Box::new(self.replace_params_with_args(args, expr)?),
+            }),
+            YolkNode::CallExpr {
+                ident,
+                args: call_args,
+            } => {
+                let mut replaced_args = Vec::new();
+                for arg in call_args.iter() {
+                    replaced_args.push(self.replace_params_with_args(args, arg)?);
+                }
+                Ok(YolkNode::CallExpr {
+                    ident: ident.to_string(),
+                    args: replaced_args,
+                })
+            }
+            YolkNode::InfixExpr { lhs, op, rhs } => Ok(YolkNode::InfixExpr {
+                lhs: Box::new(self.replace_params_with_args(args, lhs)?),
+                op: op.clone(),
+                rhs: Box::new(self.replace_params_with_args(args, rhs)?),
+            }),
+            // Replace local variables with their respective arguments
+            YolkNode::Ident(s) => {
+                let index = self.params.iter().position(|param| param == s).unwrap();
+                Ok(args[index].clone())
+            }
+            YolkNode::Array(exprs) => {
+                let mut replaced_exprs = Vec::new();
+                for expr in exprs.iter() {
+                    replaced_exprs.push(self.replace_params_with_args(args, expr)?);
+                }
+                Ok(YolkNode::Array(replaced_exprs))
+            }
+            _ => Ok(node.clone()),
+        }
     }
 }
