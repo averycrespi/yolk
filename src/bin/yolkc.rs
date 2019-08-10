2dcb9830e59a6e8e9d2f6ad541796989f2d640fd
Avery Crespi <averycrespi@gmail.com>
2019-08-10 11:25:08 -0700

    Add debug flag to yolkc
    Closes #19

--- src/bin/yolkc.rs
+++ src/bin/yolkc.rs
@@ -4,9 +4,7 @@ extern crate clap;
 use clap::{App, Arg};
 
 use yolk::ast::YololNode;
-use yolk::optimizer::optimize;
-use yolk::parser::parse;
-use yolk::transpiler::transpile;
+use yolk::{optimize, parse, transpile};
 
 use std::fs;
 
@@ -22,13 +20,29 @@ fn main() {
                 .short("i")
                 .long("infile"),
         )
+        .arg(
+            Arg::with_name("debug")
+                .help("print debug messages")
+                .long("debug"),
+        )
         .get_matches();
+
+    let debug = matches.is_present("debug");
 
     if let Some(infile) = matches.value_of("infile") {
         let source = fs::read_to_string(infile).expect("cannot read from file");
         let yolk = parse(&source).unwrap_or_else(|e| panic!("{}", e));
+        if debug {
+            eprintln!("{:?}\n", yolk);
+        }
         let (yolol, context) = transpile(&yolk).unwrap_or_else(|e| panic!("{}", e));
+        if debug {
+            eprintln!("{:?}\n", yolol);
+        }
         let optimized = optimize(&yolol, &context);
+        if debug {
+            eprintln!("{:?}\n", optimized);
+        }
         println!("{}", YololNode::format_as_program(&optimized));
     }
 }
